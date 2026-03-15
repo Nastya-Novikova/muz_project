6dd64739ed31e18298df02db124117063a1281ce
Гаврилов Саша <ugadaihto@gmail.com>
2026-03-15 18:10:21 +0700

    fix

--- backend/Models/DTOs/Favorites/FavoriteProfileDto.cs
+++ backend/Models/DTOs/Favorites/FavoriteProfileDto.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using backend.Models.DTOs.Profiles;
+using System;
 using System.Collections.Generic;
 
 namespace backend.Models.DTOs.Favorites
 {
     public class FavoriteProfileDto
     {
-        public Guid ProfileId { get; set; }
+        /*public Guid ProfileId { get; set; }
         public string FullName { get; set; } = string.Empty;
         public LookupItemDto City { get; set; } = new();
         public string? AvatarUrl { get; set; }
         public List<LookupItemDto> Genres { get; set; } = new();
         public List<LookupItemDto> Specialties { get; set; } = new();
+        public string? Description { get; set; }*/
+        public ProfileDto Profile { get; set; } = new();
         public DateTime AddedAt { get; set; }
     }
 }
--- backend/Models/DTOs/Profiles/ProfileShortDto.cs
+++ backend/Models/DTOs/Profiles/ProfileShortDto.cs
@@ -6,6 +6,9 @@
         public string FullName { get; set; } = string.Empty;
         public LookupItemDto City { get; set; } = new();
         public string? AvatarUrl { get; set; }
+        public List<LookupItemDto> Genres { get; set; } = new();
+        public List<LookupItemDto> Specialties { get; set; } = new();
+        public string? Description { get; set; }
 
         //Description
         //Genres
